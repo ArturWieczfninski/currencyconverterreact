929e3166e118aab757c7bf62a92790ae8928f877
Artur Wieczfninski <artur.wieczfninski@gmail.com>
2025-11-02 08:56:30 +0100

    deleted useOnSubmit.js

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,55 +1,75 @@
 import { useState } from "react";
 import { currencies } from "../currencies";
 import { Result } from "./Result";
-import { useOnSubmit } from "./useOnSubmit";
-import { Button, Filed, Header, Info, LabelText } from "./styled";
+import {
+  Button,
+  Filed,
+  Header,
+  Info,
+  LabelText,
+} from "./styled"
 
-export const Form = () => {
+export const Form = ({ calculateResult, result }) => {
   const [currency, setCurrency] = useState(currencies[0].short);
   const [amount, setAmount] = useState("");
-  useOnSubmit();
-  const { onSubmit, calculateResult } = useOnSubmit();
 
- 
-    return (
-      <form onSubmit={onSubmit}>
-        <Header>Przelicznik walut</Header>
-        <p>
-          <label>
-            <LabelText>Kwota w złotówkach:</LabelText>
-            <Filed
-              value={amount}
-              onChange={({ target }) => setAmount(target.value)}
-              placeholder="Wpisz kwotę w zł"
-              className="form__field"
-              type="number"
-              required
-              step="0.01"
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            <LabelText>Waluta:</LabelText>
-            <Filed
-              as="select"
-              value={currency}
-              onChange={({ target }) => setCurrency(target.value)}
-            >
-              {currencies.map((currency) => (
-                <option key={currency.short} value={currency.short}>
-                  {currency.Name}
-                </option>
-              ))}
-            </Filed>
-          </label>
-        </p>
-        <p>
-          <Button>Przelicz</Button>
-        </p>
-        <Info>Kursy pochodzą ze strony internetowej nbp.pl</Info>
-        <Result result />
-      </form>
-    );
-  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    calculateResult(currency, amount);
+  }
+
+  return (
+    <form  onSubmit={onSubmit}>
+      <Header>
+        Przelicznik walut
+        </Header>
+      <p>
+        <label>
+          <LabelText>
+            Kwota w złotówkach:
+            </LabelText>
+          <Filed        
+            value={amount}
+            onChange={({ target }) => setAmount(target.value)}
+            placeholder="Wpisz kwotę w zł"
+            className="form__field"
+            type="number"
+            required
+            step="0.01"
+          />
+            </label>
+              </p>
+              <p>
+              <label>
+          <LabelText>
+            Waluta:
+            </LabelText>
+          <Filed
+            as="select"
+            value={currency}
+            onChange={({ target }) => setCurrency(target.value)}
+          >
+            {currencies.map((currency => (
+              <option 
+              key={currency.short} 
+              value={currency.short}
+              >
+                {currency.Name}
+            </option>
+            )))}
+          </Filed>
+        </label>
+      </p>
+      <p>
+        <Button>Przelicz</Button>
+      </p>
+      <Info>
+         Kursy pochodzą ze strony internetowej nbp.pl
+      </Info>
+       <Result result={result}/>
+    </form>
+   
+     
+  );
+};
 
--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,31 @@
-import { useState } from 'react';
+
 import { Form } from './Form';
 import { Clock } from './Clock';
 import { Wrapper } from './styled';
 
 
+
 function App() {
+  const [result, setResult] = useState();
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+    .find(({ short }) => short === currency)
+    .rate;
+
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  }
   return (
     <Wrapper>
       <Clock />
-      < Form />
+      < Form
+      result={result} 
+      calculateResult={calculateResult} 
+      />
     </Wrapper>
   );
 }
