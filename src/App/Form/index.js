ab23fb8be50b824851bca4ae5fc2c1398739e0b6
Artur Wieczfninski <artur.wieczfninski@gmail.com>
2025-11-05 22:36:53 +0100

    update format

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -15,50 +15,50 @@ export const Form = () => {
   const calculateResult = (currency, amount) => {
     const rate = currencies.find(({ short }) => short === currency).rate;
 
-      setResult({
-        sourceAmount: +amount,
-        targetAmount: amount / rate,
-        currency,
-      });
-    };
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  };
 
-    return (
-      <form onSubmit={onSubmit}>
-        <Header>Przelicznik walut</Header>
-        <p>
-          <label>
-            <LabelText>Kwota w złotówkach:</LabelText>
-            <Filed
-              value={amount}
-              onChange={({ target }) => setAmount(target.value)}
-              placeholder="Wpisz kwotę w zł"
-              type="number"
-              required
-              step="0.01"
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            <LabelText>Waluta:</LabelText>
-            <Filed
-              as="select"
-              value={currency}
-              onChange={({ target }) => setCurrency(target.value)}
-            >
-              {currencies.map((currency) => (
-                <option key={currency.short} value={currency.short}>
-                  {currency.Name}
-                </option>
-              ))}
-            </Filed>
-          </label>
-        </p>
-        <p>
-          <Button>Przelicz</Button>
-        </p>
-        <Info>Kursy pochodzą ze strony internetowej nbp.pl</Info>
-        <Result result={result} />
-      </form>
-    );
+  return (
+    <form onSubmit={onSubmit}>
+      <Header>Przelicznik walut</Header>
+      <p>
+        <label>
+          <LabelText>Kwota w złotówkach:</LabelText>
+          <Filed
+            value={amount}
+            onChange={({ target }) => setAmount(target.value)}
+            placeholder="Wpisz kwotę w zł"
+            type="number"
+            required
+            step="0.01"
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          <LabelText>Waluta:</LabelText>
+          <Filed
+            as="select"
+            value={currency}
+            onChange={({ target }) => setCurrency(target.value)}
+          >
+            {currencies.map((currency) => (
+              <option key={currency.short} value={currency.short}>
+                {currency.Name}
+              </option>
+            ))}
+          </Filed>
+        </label>
+      </p>
+      <p>
+        <Button>Przelicz</Button>
+      </p>
+      <Info>Kursy pochodzą ze strony internetowej nbp.pl</Info>
+      <Result result={result} />
+    </form>
+  );
 };
