3de74a2fb4c366c6f61adc0acdf45e811ed388d8
Artur Wieczfninski <artur.wieczfninski@gmail.com>
2025-11-09 00:32:25 +0100

    Bug fixed in useRateDate.js

--- src/App/Form/Result/useRatesDate.js
+++ src/App/Form/Result/useRatesDate.js
@@ -1,33 +1,44 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
 export const useRatesDate = () => {
-    const [ratesDate, setRatesDate] = useState({
-        state: "loading",
-    });
-    useEffect(() => {
-        const fetchRatesDate = async () => {
-            try {
-                const response = await fetch('https://api.currencyapi.com/v3/latest?apikey=cur_live_eXVUMntKdJ2L4mUbmTQWf6rNyYnPQyTJkvnG96RA');
-                if (!response.ok) {
-                    throw new Error(response.statusText); 
-                }
-                const date =  await response.json();
-                console.log(date);
-                const rates = date.data;
-                setRatesDate({
-                    state: "success",
-                    rates,
-                    date,
-                });
-            } catch {
-                setRatesDate({
-                    state: "error",
-                });
-            }
+  const [ratesDate, setRatesDate] = useState({
+    state: "loading",
+  });
+
+  useEffect(() => {
+    const fetchRates = async () => {
+      try {
+        const key = "430b719ef003302046270f0f8fdf7cd6dacb";
+        const base = "USD";
+        const output = "json";
+
+        const url = `https://currencyapi.net/api/v1/rates?key=${key}&base=${base}&output=${output}`;
+        const headers = {
+          Accept: "application/json",
         };
 
-        setTimeout(fetchRatesDate, 1000 );
-    }, []);
-return ratesDate;
-};
+        const response = await fetch(url, { headers });
+        const data = await response.json();
+        console.log(data);
 
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const { rates, date } = data;
+
+        setRatesDate({
+          state: "success",
+          rates,
+          date,
+        });
+      } catch {
+        setRatesDate({
+          state: "error",
+        });
+      }
+    };
+
+    setTimeout(fetchRates, 1000);
+  }, []);
+  return ratesDate;
+};
